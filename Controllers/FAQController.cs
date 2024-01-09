1724b92356fc288a2fab990d7d2b73c133467ab9
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-01-10 03:04:10 +0500

    Updated

--- Controllers/FAQController.cs
+++ Controllers/FAQController.cs
@@ -37,7 +37,7 @@ namespace Symphony_LTD.Controllers
                 
                 return View();
             }
-
+            TempData["failed"] = "Please login!";
             return RedirectToAction("LogIn", "Admin");
         }
 
@@ -69,6 +69,78 @@ namespace Symphony_LTD.Controllers
             return View();
         }
 
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                TempData["failed"] = "Null Id selected. Select any FAQ.";
+                return RedirectToAction("Index");
+            }
 
+            if (HttpContext.Session.GetString("s_email") != null)
+            {
+                ViewBag.Email = HttpContext.Session.GetString("s_email").ToString();
+                ViewBag.Pass = HttpContext.Session.GetString("s_pass_verify").ToString();
+
+                var faq = _db.FAQS.FirstOrDefault(x => x.Id == id);
+
+                if (faq != null)
+                {
+                    ViewBag.Id = id;
+                    return View(faq);
+                }
+                TempData["failed"] = "No FAQ available on the selected Id: " + id;
+                return RedirectToAction("Index");
+            }
+
+            TempData["failed"] = "Please login!";
+            return RedirectToAction("LogIn", "Admin");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(FAQ data)
+        {
+            if (ModelState.IsValid)
+            {
+                var existing = _db.FAQS.FirstOrDefault(x => x.Id == data.Id);
+
+                if (existing != null)
+                {
+                    existing.Question = data.Question ?? existing.Question;
+                    existing.Answer = data.Answer ?? existing.Answer;
+
+                    _db.FAQS.Update(existing);
+                    _db.SaveChanges();
+                    TempData["success"] = "FAQ edited successfully!";
+                    return RedirectToAction("Index");
+                }
+
+                TempData["failed"] = "No FAQ available on the selected Id: " + data.Id;
+                return RedirectToAction("Index");
+            }
+
+            TempData["failed"] = "Model is not valid.";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete (int? id)
+        {
+            if(id != null)
+            {
+                var deleteFAQ = _db.FAQS.FirstOrDefault(x => x.Id == id);
+                if (deleteFAQ != null)
+                {
+                    _db.FAQS.Remove(deleteFAQ);
+                    _db.SaveChanges();
+                    TempData["success"] = "Successfully deleted a FAQ!";
+                    return RedirectToAction("Index");
+                }
+                TempData["failed"] = "No FAQ available on the specified ID: " + id + ". Can't delete any FAQ.";
+                return RedirectToAction("Index");
+            }
+            TempData["failed"] = "No Id Selected. Can't delete any FAQ.";
+            return RedirectToAction("Index");
+        }
     }
 }
